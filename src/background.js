0193342791fcd3d488791919527804c07f4cea2e
Parrot14 <parrot14.mail@gmail.com>
2022-05-05 21:09:47 -0500

    feat: Add socket handler (0.1.1)
    - Add websocket connection.
    
    - Add background socket.
    
    - Add parser.
    
    - Add bridge betweeb websocket and background socket.

---
+++ src/background.js
@@ -0,0 +1,56 @@
+const socket = new WebSocket( 'ws://localhost:8080/ws' )
+
+const fn_input = {
+    "01": arr =>{
+        let reason = arr[1]
+        socket.close() 
+    },
+    "02": arr =>{
+        let id = arr[1]
+        let status = arr[2]
+        browser.tabs.sendMessage( id, { type: 2, status: status } ) // Set response
+    },
+    "03": arr =>{
+        let id = arr[1]
+        let value = arr[2]
+        browser.tabs.sendMessage( id, { type: 3, value:value } ) // Get response
+    }
+}
+
+socket.addEventListener('message', event => {
+    let data = `${event.data}`;
+    let splited_data = data.split( ":" )
+    let regex = {
+       "01":/^01:[\w\s]+$/,
+       "02":/^02:(ok|error)$/,
+       "03":/^03:[a-z0-9]+$/
+    }
+    if ( regex[splited_data[0]]?.test( data ) ){
+        fn_input[splited_data[0]](splited_data)
+    }
+} )
+
+const fn_output = {
+    "02": (id, data) =>{
+        // (name,data_name,value)
+        if( socket.readyState != 1 ) {
+            browser.tabs.sendMessage( id, { type: -1 } ) // Server not ready
+        } else {
+            socket.send(`03:${id}:${data.name}:${data.data_name}:${data.value}`)
+        }
+    },
+    "03": (id, data) =>{
+        // (id,name,data_name)
+        if( socket.readyState != 1 ) {
+            browser.tabs.sendMessage( id, { type: -1 } ) // Server not ready
+        } else {
+            socket.send(`03:${id}:${data.name}:${data.data_name}`)
+        }
+    }
+}
+
+browser.runtime.onMessage.addListener( ( data, sender ) => {
+        let id = sender.tab.id
+        fn_output[ data.type ]?.( id, data )
+    }
+);
